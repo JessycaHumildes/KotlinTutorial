aaab84d52c2b01cb8f1a14b277d000309939c98d
JessycaHumildes <jessycahumildes@hotmail.com>
2020-09-14 13:29:14 +0100

    Initial commit

---
+++ app/src/main/java/Main.kt
@@ -0,0 +1,40 @@
+fun main(args: Array<String>) {
+
+    val tim = Player("Tim")
+//    println(tim.name)
+//    println(tim.lives)
+//    println(tim.level)
+//    println(tim.score)
+
+    tim.Show()
+
+    val louise = Player("Louise", 5)
+    louise.Show()
+
+    val newP = Player("newP", 4, 8)
+    val newp2 = Player("newp2", 2, 5, 1000)
+    newP.Show()
+    newp2.Show()
+    println(newP.weapon.name.toUpperCase())
+    println(newP.weapon.damageInflicted)
+
+    val axe = Weapon("Axe", 2)
+    newp2.weapon= axe
+    println(newp2.weapon.name)
+    println(axe.name)
+
+    axe.damageInflicted=24
+    println(axe.damageInflicted)
+    println(newp2.weapon.damageInflicted)
+
+    tim.weapon = Weapon("Sword", 12)
+//    println(tim.weapon.name)
+    tim.Show()
+
+    louise.weapon = tim.weapon
+    louise.Show()
+
+    tim.weapon = Weapon("Spear", 10)
+//    println(tim.weapon.name)
+    tim.Show()
+}
